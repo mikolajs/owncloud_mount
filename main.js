e96a508201b94aafee75097e5f211b8e5ff3b546
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2012-09-08 16:11:28 +0200

    Read file secret

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env seed
 
 Gtk = imports.gi.Gtk;
+GIO = imports.gi.Gio;
 Gtk.init(Seed.argv);
 GtkBuilder = imports.gtkbuilder;
 GObject = imports.gi.GObject;
@@ -11,34 +12,36 @@ var isMounted = false;
 var builder = new Gtk.Builder();
 builder.add_from_file(UI_FILE);
 var window = builder.get_object("window");
-print(window);
-var button_mount = builder.get_object("button1");
-var button_unmount = builder.get_object("button2");
+window.signal.hide.connect(Gtk.main_quit);
+
+var button = builder.get_object("button");
 var label_info = builder.get_object("label1");
+var text_server = builder.get_object("entry0");
 var text_user = builder.get_object("entry1");
 var text_password = builder.get_object("entry2");
 
+function readSecretFile(){
+	var input_file = GIO.file_new_for_path("/home/ms/.davfs2/secrets")
+    var fstream = input_file.read();
+    var dstream = new GIO.DataInputStream.c_new(fstream);
+    var line = dstream.read_until("", 0);
+    fstream.close();
+	print(line);
+	return line;
+}
 
+var secretEntryLine =  readSecretFile();
 
-
-
-button_unmount.signal.clicked.connect(function(b){
+button.signal.clicked.connect(function(b){
       if(isMounted){
+		
         print("UNMOUNTED");
         isMounted = false;
     } 
 });
 
-button_mount.signal.clicked.connect(function (b){
-    if(!isMounted) {
-    print("MOUNTED");
-    isMounted = true;
-    }
-});
 
 window.show_all();
-
-
 Gtk.main();
 
 
